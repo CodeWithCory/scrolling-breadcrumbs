25ee733b23bbd0687a4b2c82f7efc4f40e0cf2d3
CoryLR <cory.rahman@gmail.com>
2023-01-18 16:03:21 -0500

    Save progress

---
+++ lib/scrolling-breadcrumbs.ts
@@ -0,0 +1,21 @@
+
+
+export function scrollingBreadcrumbs(selector: string, options: any) {
+  
+}
+
+
+
+
+
+/* Scratch */
+
+// const breadcrumbs = document.querySelector('.breadcrumbs');
+// const breadcrumbsContainer = document.querySelector('.breadcrumbs-container');
+// const breadcrumbsContainerWidth = breadcrumbsContainer.getBoundingClientRect().width;
+// const breadcrumbsWidth = breadcrumbs.getBoundingClientRect().width;
+// const breadcrumbsOffset = breadcrumbs.getBoundingClientRect().left;
+
+// if (breadcrumbsWidth > breadcrumbsContainerWidth) {
+//   breadcrumbsContainer.scrollLeft = breadcrumbsOffset;
+// }
